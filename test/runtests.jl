8bc411b018fe2ebba581ddef297a7e54c57dd229
Takafumi Arakaki <aka.tkf@gmail.com>
2019-01-28 00:30:00 -0800

    Run detect_ambiguities in test_broken

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,4 +4,5 @@ using Test
 @testset "Tofu.jl" begin
     @test (◻.x)((x=1,)) == 1
     @test (◻.x + 2)((x=1,)) == 3
+    @test_broken detect_ambiguities(Tofu, Base, imported=true, recursive=true) == []
 end
