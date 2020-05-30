999665f5f72b4b88d7dfd52edc54544c5a70d636
Takafumi Arakaki <aka.tkf@gmail.com>
2020-05-29 19:31:47 -0700

    More tests

--- src/UnderscoreOh.jl
+++ src/UnderscoreOh.jl
@@ -32,11 +32,32 @@ Base.BroadcastStyle(::UnderscoreOhStyle, ::Broadcast.BroadcastStyle) = Underscor
 Base.broadcastable(x::Graph) = x
 Broadcast.instantiate(bc::Broadcasted{UnderscoreOhStyle}) = bc
 
+# Used only for `KWBroadcastedInner` below:
+function air end
+struct Aired{T}
+    value::T
+end
+Broadcast.broadcasted(::typeof(air), x) = Aired(x)
+Broadcast.materialize(x::Aired) = x.value
+
+# A hack to support broadcasting with keyword arguments:
+const KWBroadcastedInner = let T = typeof(air.(identity.(; dummy = 1)).f)
+    getfield(parentmodule(T), nameof(T))
+end
+
 # Convert broadcasted to a callable
 Base.copy(bc::Broadcasted{UnderscoreOhStyle}) = asgraph(bc)
 
 asgraph(x) = x
-asgraph(bc::Broadcasted) = call(bc.f, map(asgraph, bc.args)...)
+function asgraph(bc::Broadcasted)
+    if bc.f isa KWBroadcastedInner
+        args = map(asgraph, bc.args)
+        kwargs = _map(asgraph, (; bc.f.kwargs...))
+        return call(bc.f.f, args...; kwargs...)
+    else
+        return call(bc.f, map(asgraph, bc.args)...)
+    end
+end
 
 function _nt(; kwargs...)
     if any(x -> x isa Graph, values(kwargs))
@@ -66,8 +87,7 @@ _map(f, xs::NamedTuple{names}) where {names} = NamedTuple{names}(map(f, Tuple(xs
 
 # --- Show it like you build it
 
-maybe_print_dot(io) =
-    get(io, :_secrete_print_dot_key, true) ? print(io, '.') : nothing
+maybe_print_dot(io) = get(io, :_secrete_print_dot_key, true) ? print(io, '.') : nothing
 unset_print_dot(io) = IOContext(io, :_secrete_print_dot_key => false)
 
 @nospecialize
@@ -77,7 +97,9 @@ Base.show(io::IO, g::Call) = show_impl(io, _f(g), _args(g), _kwargs(g))
 
 is_binop(f) = Base.isbinaryoperator(nameof(f))
 
-function show_impl(io, f, args, kwargs)
+show_impl(io, f, args, kwargs) = show_impl_default(io, f, args, kwargs)
+
+function show_impl_default(io, f, args, kwargs)
     if length(args) > 0 && isempty(kwargs) && is_binop(f)
         # Print as binary operator:
         show_term(io, f, args[1])
@@ -89,7 +111,9 @@ function show_impl(io, f, args, kwargs)
         end
         return
     end
-    print(io, f, '(')
+    print(io, f)
+    maybe_print_dot(io)
+    print(io, '(')
     if length(args) > 0
         show(io, args[1])
         for a in args[2:end]
@@ -127,12 +151,22 @@ need_paren(f, g) = false
 need_paren(f, g::Call) = is_binop(_f(g)) && _f(g) !== f
 
 function show_impl(io, ::typeof(getproperty), args, kwargs)
-    value, (name::Symbol) = args
-    @assert length(args) == 2
-    @assert length(kwargs) == 0
-    show(io, value)
-    print(io, '.')
-    print(io, name)
+    if length(args) == 2 && length(kwargs) == 0
+        value, prop = args
+        if prop isa String
+            show(io, value)
+            print(io, '.')
+            show(io, prop)
+            return
+        end
+        tmpstr = string(Expr(:., :_, QuoteNode(prop)))
+        if startswith(tmpstr, "_.")
+            show(io, value)
+            print(io, tmpstr[2:end])
+            return
+        end
+    end
+    show_impl_default(io, getproperty, args, kwargs)
     return
 end
 
@@ -152,8 +186,17 @@ function show_impl(io, ::typeof(getindex), args, kwargs)
 end
 
 function Base.show(io::IO, ::MIME"text/plain", g::Graph)
+    if get(io, :compact, false) === true
+        printstyled(io, '('; color = :light_black)
+        show(io, g)
+        printstyled(io, ')'; color = :light_black)
+        return
+    end
     print(io, _o, " -> ")
     show(unset_print_dot(io), g)
+    n = length(methods(g))
+    m = n == 1 ? "method" : "methods"
+    print(io, " (generic function with $n $m)")
 end
 
 # `Base.Function` defines those methods to be something different
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,57 @@
+module TestUnderscoreOh
+
 using UnderscoreOh
 using Test
 
-@testset "UnderscoreOh.jl" begin
-    @test (_o.x)((x=1,)) == 1
-    @test (_o.x .+ 2)((x=1,)) == 3
+struct IdentityProperties end
+Base.getproperty(::IdentityProperties, x::Symbol) = x
+Base.getproperty(::IdentityProperties, x) = x
+
+@testset "eval" begin
+    @test (_o.x)((x = 1,)) == 1
+    @test (_o.x .+ 2)((x = 1,)) == 3
+    @test (_o[2])((11, 22)) == 22
+    @test (_o.x[2])((x = (11, 22),)) == 22
+    @test (_o.p)(IdentityProperties()) == :p
+    @test (_o."p")(IdentityProperties()) == "p"
+    @test (_o.x[2]."p")((x = (11, IdentityProperties()),)) == "p"
+    @test _nt(X = (_o.x))((x = 1,)) == (X = 1,)
 end
+
+const DATASET_REPR = [
+    # Desired `repr(_)` outputs:
+    "_o",
+    "_o.x",
+    "_o.\"x\"",
+    "_o .* 2",
+    "getproperty.(_o, 1, 2, 3)",
+    "getproperty.(_o, 1; k=1)",
+    "2 .* _o .* 2",
+    "(2 .* _o .* 2) .+ 0",
+    "2 .* _o",
+    "2 .* _o[1]",
+    "2 .* _o.x[1]",
+    "2 .* _o.x[1].a",
+    "2 .* identity.(_o.x[1]).a",
+    "identity.(2 .* _o.x[1]).a",
+]
+
+sshow3(x; kwargs...) = sprint(show, MIME"text/plain"(), x; kwargs...)
+
+@testset "show" begin
+    @testset for code in DATASET_REPR
+        @test repr(include_string(@__MODULE__, code)) == code
+    end
+    suffix = "(generic function with 1 method)"
+    @test sshow3(_o) == "_o -> _o $suffix"
+    @test sshow3(_o .* 2) == "_o -> _o * 2 $suffix"
+    @test sshow3(_o[1].x .* 2) == "_o -> _o[1].x * 2 $suffix"
+    @test sshow3(_o; context = :compact => true) == "(_o)"
+    @test sshow3(2 .* _o; context = :compact => true) == "(2 .* _o)"
+    @testset "smoketest" begin
+        @test !isempty(repr(getproperty(_o, nothing))::AbstractString)
+        @test !isempty(repr(getproperty(_o, [1, 2]))::AbstractString)
+    end
+end
+
+end  # module
